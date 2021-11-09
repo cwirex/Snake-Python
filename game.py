7482aba4d8f693f15c80b065e2d37ca8103912f1
cwirex <ocwiejamateusz@gmail.com>
2021-11-09 13:17:40 +0100

    Init Classes

---
+++ game.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.locals import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Snake - Python 3.8')
---
+++ snake.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.locals import *
+
+class Snake:
+    def __init__(self):
+        pass
+
