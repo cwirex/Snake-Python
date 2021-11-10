e9c56e1d83c0966372cde951681f939474c2784f
cwirex <ocwiejamateusz@gmail.com>
2021-11-10 10:56:28 +0100

    Create test

---
+++ src/Tests/test_snake.py
@@ -0,0 +1,29 @@
+import unittest
+from src.snake import *
+
+
+class TestSnake(unittest.TestCase):
+    def setUp(self):
+        self.snake = Snake(pygame.display.set_mode((900, 600)))
+
+    def test_init(self):
+        self.assertIsNotNone(self.snake)
+
+    def test_score(self):
+        self.assertEqual(0, self.snake.get_score())
+        self.snake.body.append([0, 0])
+        self.assertEqual(1, self.snake.get_score())
+        self.snake.body.append([0, 1])
+        self.assertEqual(2, self.snake.get_score())
+
+    def test_key_pressed(self):
+        first = [self.snake.body[0][0], self.snake.body[0][1]]
+        self.assertNotEqual(first, self.snake.body[1])
+        self.snake.key_pressed(K_DOWN)
+        self.assertEqual(first, self.snake.body[1])
+        first[1] += 1
+        self.assertEqual(first, self.snake.body[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
